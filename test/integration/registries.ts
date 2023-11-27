6ca919f29241ec8e532790f86279c454d445f7b3
hasundue <hasundue@gmail.com>
2023-11-28 08:19:07 +0900

    test(registries): add a step to validate URL

--- test/integration/registries.ts
+++ test/integration/registries.ts
@@ -3,80 +3,65 @@ import { parse, resolveLatestVersion } from "../../lib/dependency.ts";
 
 type RegistryTestSpec = [
   name: string,
-  versioned: string,
-  unversioned: string,
+  url: string,
 ];
 
 const SPECS = [
   [
     "deno.land/std",
-    "https://deno.land/std@0.200.0/fs/mod.ts",
-    "https://deno.land/std/fs/mod.ts",
+    "https://deno.land/std@0.200.0/version.ts",
   ],
   [
     "deno.land/x",
-    "https://deno.land/x/molt@0.1.0/mod.ts",
-    "https://deno.land/x/molt/mod.ts",
+    "https://deno.land/x/molt@0.14.0/lib/path.ts",
   ],
   [
     "npm:",
-    "npm:typescript@4.1.2",
-    "npm:typescript",
-  ],
-  [
-    "cdn.jsdelivr.net/gh",
-    "https://cdn.jsdelivr.net/gh/hasundue/molt@e4509a9/mod.ts",
-    "https://cdn.jsdelivr.net/gh/hasundue/molt/mod.ts",
+    "npm:preact@10.5.5",
   ],
   [
     "cdn.jsdelivr.net/npm",
-    "https://cdn.jsdelivr.net/npm/react@16.7.0",
-    "https://cdn.jsdelivr.net/npm/react",
+    "https://cdn.jsdelivr.net/npm/preact@10.5.5",
+  ],
+  [
+    "cdn.jsdelivr.net/gh",
+    "https://cdn.jsdelivr.net/gh/hasundue/molt@e4509a9/lib/path.ts",
   ],
   [
     "cdn.skypack.dev",
     "https://cdn.skypack.dev/preact@10.5.5",
-    "https://cdn.skypack.dev/preact",
   ],
   [
     "denopkg.com",
-    "https://denopkg.com/hasundue/molt@0.1.0/mod.ts",
-    "https://denopkg.com/hasundue/molt/mod.ts",
+    "https://denopkg.com/hasundue/molt@e4509a9/lib/path.ts",
   ],
   [
-    "dev.jspm.io",
-    "https://dev.jspm.io/npm:react@16.7.0",
-    "https://dev.jspm.io/npm:react",
+    "ga.jspm.io",
+    "https://ga.jspm.io/npm:lit-html@2.7.0/development/is-server.js",
   ],
   [
     "esm.run",
-    "https://esm.run/d3@7.8.0",
-    "https://esm.run/d3",
+    "https://esm.run/preact@10.5.5",
   ],
   [
     "esm.sh",
-    "https://esm.sh/react@16.7.0",
-    "https://esm.sh/react",
+    "https://esm.sh/preact@10.5.5",
   ],
   [
     "x.nest.land",
     "https://x.nest.land/parsec@0.1.0/mod.ts",
-    "https://x.nest.land/parsec/mod.ts",
   ],
   [
     "pax.deno.dev",
-    "https://pax.deno.dev/hasundue/molt@0.1.0/mod.ts",
-    "https://pax.deno.dev/hasundue/molt/mod.ts",
+    "https://pax.deno.dev/hasundue/molt@e4509a9/lib/path.ts",
   ],
   [
     "raw.githubusercontent.com",
-    "https://raw.githubusercontent.com/hasundue/molt/e4509a9/mod.ts",
-    "https://raw.githubusercontent.com/hasundue/molt/main/mod.ts",
+    "https://raw.githubusercontent.com/hasundue/molt/e4509a9/lib/path.ts",
   ],
   [
     "unpkg.com",
-    "https://unpkg.com/react@16.7.0/umd/react.production.min.js",
-    "https://unpkg.com/react/umd/react.production.min.js",
+    "https://unpkg.com/preact@10.5.5",
   ],
 ] satisfies RegistryTestSpec[];
 
@@ -86,10 +71,20 @@ for (const spec of SPECS) {
   Deno.test(name, async (t) => {
     const assert = (it: unknown) => assertSnapshot(t, it !== undefined);
 
-    const versioned = parse(spec[1]);
+    // Make sure the URL is valid.
+    // Skip this test for npm because it fails.
+    if (name !== "npm:") {
+      await assert(await import(spec[1]));
+    } else {
+      await assert(true);
+    }
 
-    await assert(versioned.version);
-    await assert(await resolveLatestVersion(versioned));
-    await assert(await resolveLatestVersion(parse(spec[2])));
+    // Check if the dependency can be parsed.
+    const parsed = parse(spec[1]);
+    await assert(parsed.version);
+
+    // Check if the latest version can be resolved.
+    const updated = await resolveLatestVersion(parsed);
+    await assert(updated);
   });
 }
