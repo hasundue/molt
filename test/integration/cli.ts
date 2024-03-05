79e095c45d6e9a683d8bc861338ad9d15239e8ee
hasundue <hasundue@gmail.com>
2024-03-05 09:22:48 +0900

    test(cli): refactor test implementation

--- test/integration/cli.ts
+++ test/integration/cli.ts
@@ -1,6 +1,91 @@
 import { assertEquals } from "../../lib/std/assert.ts";
 import { stripAnsiCode } from "../../lib/std/fmt.ts";
+import { join } from "../../lib/std/path.ts";
 import { assertSnapshot } from "../../lib/testing.ts";
+
+// basic commands
+molt("", { code: 2 });
+molt("--help");
+molt("--version");
+
+// single file
+molt("not_exist.ts", { code: 1 });
+molt("import.ts");
+
+// special registries
+molt("jsr.ts");
+
+// import maps
+molt("mod.ts", { cwd: "import_map" });
+molt("mod.ts --import-map deno.json", { cwd: "import_map" });
+molt("deno.json", { cwd: "import_map", code: 1 });
+
+// --ignore and --only
+molt("multiple_imports.ts --ignore node-emoji");
+molt("multiple_imports.ts --ignore=deno_graph,node-emoji");
+molt("multiple_imports.ts --only deno.land/std");
+molt("multiple_imports.ts --only=deno.land/std,deno_graph");
+molt("multiple_imports.ts --only deno.land --ignore deno_graph");
+
+// --write
+molt("mod.ts --write", { cwd: "multiple_modules" });
+molt(
+  "mod.ts --write --summary title.txt --report body.md",
+  { cwd: "multiple_modules" },
+);
+
+// --commit
+molt("mod.ts --commit", { cwd: "multiple_modules" });
+molt("mod.ts --commit --prefix :package:", { cwd: "multiple_modules" });
+molt(
+  "mod.ts --commit --pre-commit=fmt --post-commit=lint",
+  { cwd: "multiple_modules" },
+);
+molt(
+  "mod.ts --commit --summary title.txt --report body.md",
+  { cwd: "multiple_modules" },
+);
+molt(
+  "mod.ts --commit --summary title.txt --pre-commit=fmt",
+  { cwd: "multiple_modules" },
+);
+
+//-----------------------
+// Test implementation
+//-----------------------
+
+const BIN = new URL("../../cli.ts", import.meta.url).pathname;
+const DATA = new URL("../data", import.meta.url).pathname;
+
+function molt(line: string, opts?: {
+  cwd?: string;
+  code?: number;
+}) {
+  const args = line.length > 0 ? line.split(" ") : [];
+
+  const name = "cli - " + (opts?.cwd ? opts.cwd + " - " : "") +
+    '"' + ["molt"].concat(args).join(" ") + '"';
+
+  Deno.test(name, async (t) => {
+    const output = await new Deno.Command("deno", {
+      args: ["run", "-A", BIN, ...args],
+      env: { MOLT_TEST: "1" },
+      cwd: join(DATA, opts?.cwd ? `/${opts?.cwd}` : ""),
+    }).output();
+
+    const stdout = stringify(output.stdout);
+    const stderr = stringify(output.stderr);
+    try {
+      assertEquals(output.code, opts?.code ?? 0);
+    } catch (err) {
+      console.error(stdout);
+      console.error(stderr);
+      throw err;
+    }
+    await assertSnapshot(t, stdout);
+    await assertSnapshot(t, stderr);
+  });
+}
 
 function stringify(data: Uint8Array) {
   const decoder = new TextDecoder();
@@ -12,68 +97,3 @@ function stringify(data: Uint8Array) {
   }
   return stripAnsiCode(text);
 }
-
-function test(cmd: string, code = 0) {
-  const [, ...args] = cmd.split(" ");
-  Deno.test(cmd, async (t) => {
-    const output = await new Deno.Command("deno", {
-      args: ["run", "-A", "./cli.ts", ...args],
-      env: { MOLT_TEST: "1" },
-    }).output();
-    const stdout = stringify(output.stdout);
-    const stderr = stringify(output.stderr);
-    try {
-      assertEquals(output.code, code);
-    } catch (err) {
-      console.error(stdout);
-      console.error(stderr);
-      throw err;
-    }
-    await assertSnapshot(t, stdout);
-    await assertSnapshot(t, stderr);
-  });
-}
-
-const dir = "test/data";
-
-test("molt", 2);
-test("molt --help");
-test("molt --version");
-
-test(`molt not_exist.ts`, 1);
-test(`molt ${dir}/import.ts`);
-
-test(`molt ${dir}/jsr.ts`);
-
-test(`molt ${dir}/import_map/mod.ts`);
-test(`molt ${dir}/import_map/mod.ts --import-map ${dir}/import_map/deno.json`);
-test(`molt ${dir}/import_map/deno.json`, 1);
-test(
-  `molt ${dir}/import_map/mod.ts --import-map not_exist.json`,
-  1,
-);
-
-test(`molt ${dir}/multiple_imports.ts --ignore node-emoji`);
-test(`molt ${dir}/multiple_imports.ts --ignore=deno_graph,node-emoji`);
-
-test(`molt ${dir}/multiple_imports.ts --only deno.land/std`);
-test(`molt ${dir}/multiple_imports.ts --only=deno.land/std,deno_graph`);
-
-test(`molt ${dir}/multiple_imports.ts --only deno.land --ignore deno_graph`);
-
-test(`molt ${dir}/multiple_modules/mod.ts --write`);
-test(
-  `molt ${dir}/multiple_modules/mod.ts --write --summary title.txt --report body.md`,
-);
-
-test(`molt ${dir}/multiple_modules/mod.ts --commit`);
-test(`molt ${dir}/multiple_modules/mod.ts --commit --prefix :package:`);
-test(
-  `molt ${dir}/multiple_modules/mod.ts --commit --pre-commit=fmt --post-commit=lint`,
-);
-test(
-  `molt ${dir}/multiple_modules/mod.ts --commit --summary title.txt --report body.md`,
-);
-test(
-  `molt ${dir}/multiple_modules/mod.ts --commit --summary title.txt --pre-commit=fmt`,
-);
