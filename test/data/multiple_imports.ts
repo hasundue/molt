3d8031ff6d460b72d474dc0d25811256d3f9398b
hasundue <hasundue@gmail.com>
2024-03-05 13:37:55 +0900

    test: add `jsr:` imports in some test cases

--- test/data/multiple_imports.ts
+++ test/data/multiple_imports.ts
@@ -1,3 +1,4 @@
 import { assert } from "https://deno.land/std@0.200.0/assert/assert.ts";
 import { createGraph } from "https://deno.land/x/deno_graph@0.50.0/mod.ts";
 import emoji from "npm:node-emoji@2.0.0";
+import { printProgress } from "jsr:@luca/flag@1.0.0";
