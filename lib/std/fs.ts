92515f904ba1167072696ffb9f9e21292bcc6285
hasundue <hasundue@gmail.com>
2024-03-06 12:59:00 +0900

    fix: add EOL at the end of a JSON file

--- lib/std/fs.ts
+++ lib/std/fs.ts
@@ -1,6 +1,7 @@
 export { existsSync } from "https://deno.land/std@0.218.2/fs/exists.ts";
 export {
   detect as detectEOL,
+  EOL,
   format as formatEOL,
   LF,
 } from "https://deno.land/std@0.218.2/fs/eol.ts";
