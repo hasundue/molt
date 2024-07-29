98f768838b28071490576307dac2c84add8b16eb
hasundue <hasundue@gmail.com>
2024-07-29 14:07:06 +0900

    chore: abandon `test` directory under the root

--- test/fixtures/lockfile/mod.ts
+++
@@ -1,3 +0,0 @@
-import { copy } from "std/bytes";
-import { App } from "hono";
-import { match } from "@core/match";
--- test/fixtures/mod.ts
+++
@@ -1,2 +0,0 @@
-// http - export
-export { createGraph } from "https://deno.land/x/deno_graph@0.50.0/mod.ts";
