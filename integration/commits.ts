4f4ced4e512e3b55c959c8eccd6987274193d078
hasundue <hasundue@gmail.com>
2024-08-02 17:15:47 +0900

    fix(integration): do not error on non-existing tag

--- integration/commits.ts
+++ integration/commits.ts
@@ -1,43 +1,57 @@
 import type { Repository } from "./repository.ts";
 import * as github from "./github.ts";
+import { equals, parse } from "@std/semver";
 
 /**
  * Get the commits between two Git references in the repository.
+ * If the tags for the specified versions are not found, an empty array is returned.
+ *
+ * @param repo The repository to fetch the commits from.
+ * @param from The version of the dependency to compare from.
+ * @param to The version of the dependency to compare to.
+ *
+ * @returns The commit messages between the two versions.
  */
 export async function compareCommits(
   repo: Repository,
-  base: string,
-  head: string,
+  from: string,
+  to: string,
 ): Promise<string[]> {
   //
   // Return the stored commits if available
   //
   using kv = await Deno.openKv();
-  const stored = await kv.get<string[]>([
-    "commits",
-    repo.owner,
-    repo.name,
-    base,
-    head,
-  ]);
+  const stored = await kv.get<string[]>(
+    ["commits", repo.owner, repo.name, from, to],
+  );
   if (stored.value) {
     return stored.value;
   }
   //
   // Otherwise, fetch the commits from the Git hosting platform
   //
-  const commits = await _compareCommits(repo, base, head);
+  const tags = await _getTags(repo);
+  // Tags and versions not necessarily include the "v" prefix consistently
+  const base = tags.find((it) => equals(parse(it), parse(from)));
+  const head = tags.find((it) => equals(parse(it), parse(to)));
+  if (!base || !head) {
+    return [];
+  }
+  const commits = await _compareCommits(repo, from, to);
   await kv.set(
-    [
-      "commits",
-      repo.owner,
-      repo.name,
-      base,
-      head,
-    ],
+    ["commits", repo.owner, repo.name, from, to],
     commits,
   );
   return commits;
+}
+
+async function _getTags(repo: Repository): Promise<string[]> {
+  switch (repo.host) {
+    case "github":
+      return await github.getTags(repo);
+    default:
+      throw new Error(`Unsupported Git hosting platform: ${repo.host}`);
+  }
 }
 
 async function _compareCommits(
